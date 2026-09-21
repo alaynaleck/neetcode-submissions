be4f0aaeab7b54be1ca6d253a5f8ba8f87ae9a7c
Alayna Leck <alaynaleck@gmail.com>
2026-09-21 15:08:06 -0500

    Add: rotting-fruit - submission-4

---
+++ Data Structures & Algorithms/rotting-fruit/submission-4.py
@@ -0,0 +1,36 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        queue = deque()
+        fresh_fruit = 0
+        minutes = 0
+        
+        # Add each rotting fruit to a queue for BFS
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 2:
+                    queue.append((row, col, minutes))
+                elif grid[row][col] == 1:
+                    fresh_fruit += 1
+
+        def add_fruit(row, col, minutes):
+            nonlocal fresh_fruit
+            if (row >= 0 and row < len(grid) 
+                and col >= 0 and col < len(grid[0])
+                and grid[row][col] == 1):
+                queue.append((row, col, minutes))
+                grid[row][col] = 2
+                fresh_fruit -= 1
+        
+        # Multi-way bfs to track how much time for each fruit
+        while queue:
+            row, col, minutes = queue.popleft()
+            
+            add_fruit(row - 1, col, minutes + 1)
+            add_fruit(row + 1, col, minutes + 1)
+            add_fruit(row, col - 1, minutes + 1)
+            add_fruit(row, col + 1, minutes + 1)
+
+        return minutes if fresh_fruit == 0 else -1
+
+
+    
