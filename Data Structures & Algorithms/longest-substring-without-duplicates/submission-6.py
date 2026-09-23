906a4bc60b8c925593190cf138a41cdfd29a2728
Alayna Leck <alaynaleck@gmail.com>
2026-09-23 13:17:55 -0500

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # p: start = -1, last_seen = -1, current = 0 - -1 = 1, p = 0
+        # w: start = -1, last_seen = -1, current = 1 - -1 = 2, w = 1
+        # w: start = -1, last_seen = 1, start = 1, current = 2 - 1, w = 2
+        # 
+
+        # we need to clear everything up to that last index
+        # seen = set()
+        # OH two pointer? 
+        longest, current = 0, 0
+        start = -1
+        seen = {}
+        for i in range(len(s)):
+            last_seen = seen.get(s[i], -1)
+            if last_seen > start:
+                start = last_seen
+            current = i - start
+            seen[s[i]] = i  
+            longest = max(longest, current)
+        return longest
+
